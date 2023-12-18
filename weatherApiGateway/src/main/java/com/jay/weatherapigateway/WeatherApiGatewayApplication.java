d82601b73dd5245d0445139e7c1ec893c4ab3472
Jaydeo Sahu <Jaydeo.Sahu@cognizant.com>
2023-12-19 00:51:39 +0530

    jwt validation required

---
+++ weatherApiGateway/src/main/java/com/jay/weatherapigateway/WeatherApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jay.weatherapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiGatewayApplication.class, args);
+	}
+
+}
---
+++ weatherApiGateway/src/test/java/com/jay/weatherapigateway/WeatherApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.weatherapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
