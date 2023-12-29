5b9f6d522a2f64ea6d29ff9eb1a9eef26902f56f
Jaydeo Sahu <Jaydeo.Sahu@cognizant.com>
2023-12-29 23:30:02 +0530

    design : header nav component pending

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes></Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
