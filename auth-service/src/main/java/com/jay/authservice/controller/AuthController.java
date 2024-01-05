ccc2f57635b24caf4f2331dc8ee17135ab9b3d15
Jaydeo Sahu <Jaydeo.Sahu@cognizant.com>
2024-01-05 18:22:25 +0530

    fix: change to post mapping

--- auth-service/src/main/java/com/jay/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/jay/authservice/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,11 @@ import com.jay.authservice.exception.LoginException;
 import com.jay.authservice.jwt.JWTGenerator;
 import com.jay.authservice.service.AuthServiceImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/auth")
+@Slf4j
 public class AuthController {
 	private AuthServiceImpl authService;
 	private JWTGenerator jwtGenerator;
@@ -29,16 +33,19 @@ public class AuthController {
 		this.jwtGenerator = jwtGenerator;
 	}
 
-	@GetMapping("/login")
+	@PostMapping("/login")
 	public ResponseEntity<SuccessResponse> login(@RequestBody LoginDetails loginDetails) throws LoginException {
 		try {
+			log.info("performing login");
 			authenticationManager.authenticate(
 					new UsernamePasswordAuthenticationToken(loginDetails.getUsername(), loginDetails.getPassword()));
 		} catch (UsernameNotFoundException exception) {
+			log.info("login failed");
 			throw new LoginException("Enter correct username or password");
 		}
 		UserDetails userDetails = authService.loadUserByUsername(loginDetails.getUsername());
 		String token = jwtGenerator.generateToken(userDetails.getUsername());
+		log.info("token generated {}",token);
 		return ResponseEntity.ok(new SuccessResponse(userDetails.getUsername(),token));
 	}
 }
--- weather-service/src/main/java/com/jay/weatherservice/controller/WeatherController.java
+++ weather-service/src/main/java/com/jay/weatherservice/controller/WeatherController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class WeatherController {
 	public ResponseEntity<List<Location>> search(@PathVariable String city) {
 		return ResponseEntity.ok(weatherService.search(city));
 	}
-	@GetMapping("/current")
+	@PostMapping("/current")
 	public ResponseEntity<WeatherResponse> getWeather(@RequestBody Coord coordinates){
 		return ResponseEntity.ok(weatherService.getWeather(coordinates));
 	}
