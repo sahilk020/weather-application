29daa09ac8889daf6d6b619e1c3dc784786580ca
Jaydeo Sahu <Jaydeo.Sahu@cognizant.com>
2024-01-22 13:14:03 +0530

    modified handler

--- auth-service/src/main/java/com/jay/authservice/exceptionHandler/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/jay/authservice/exceptionHandler/GlobalExceptionHandler.java
@@ -2,16 +2,16 @@ package com.jay.authservice.exceptionHandler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.jay.authservice.entity.ErrorResponse;
 import com.jay.authservice.exception.LoginException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(LoginException.class)
 	public ResponseEntity<ErrorResponse> handleLoginException(LoginException loginException){
-		return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST,loginException.getMessage()));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST,loginException.getMessage()));
 	}
 }
